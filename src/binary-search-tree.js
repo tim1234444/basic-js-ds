7530d4868c616a63c7b6ee1c74972c42defa32f1
tim1234444 <petruhintimofej@gmail.com>
2025-01-06 20:00:58 +0300

    commit 2

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -6,27 +6,68 @@ const { NotImplementedError } = require('../extensions/index.js');
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
 class BinarySearchTree {
-
+  constructor() {
+    this._root = null;
+  }
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this._root;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    const newNode = new Node(data);
+    if (this._root === null) {
+      this._root = newNode;
+    } else {
+      this._insertNode(this._root, newNode);
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  _insertNode(node, newNode) {
+    if (newNode.data < node.data) {
+      if (node.left === null) {
+        node.left = newNode;
+      } else {
+        this._insertNode(node.left, newNode);
+      }
+    } else {
+      if (node.right === null) {
+        node.right = newNode;
+      } else {
+        this._insertNode(node.right, newNode);
+      }
+    }
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    return this._search(this._root, data) !== null;
   }
+
+  find(data) {
+    return this._search(this._root, data);
+  }
+
+  _search(node, data) {
+    if (node === null) {
+      return null;
+    }
+    if (data === node.data) {
+      return node;
+    } else if (data < node.data) {
+      return this._search(node.left, data);
+    } else {
+      return this._search(node.right, data);
+    }
+  }
+
 
   remove(/* data */) {
     throw new NotImplementedError('Not implemented');
